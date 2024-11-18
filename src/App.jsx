078cf71eff34c1bc1a56771f4991dbd9754fa0e7
mfarhann10 <mfarhann1009@gmail.com>
2024-11-19 06:11:55 +0700

    add logic to Stats components

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,10 +1,26 @@
-function Stats() {
+/* eslint-disable react/prop-types */
+function Stats({ items }) {
+  const footer =
+    "bg-green-500 text-white text-center py-6 rounded-lg shadow-lg mx-4";
+
+  if (!items.length)
+    return (
+      <p className={footer}>
+        <em className="text-lg font-semibold text-white">
+          Start adding some items to your packing list 🚀
+        </em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const numTotalPercent = Math.round((numPacked / numItems) * 100);
   return (
-    <footer className="bg-green-500 text-white text-center py-6 rounded-lg shadow-lg mx-4">
-      <em className="text-lg font-semibold">
-        You have <span className="text-yellow-300">X</span> items on your list,
-        and you already packed <span className="text-yellow-300">X</span>(
-        <span className="text-yellow-300">X%</span>)
+    <footer className={footer}>
+      <em className="text-lg font-semibold text-white">
+        {numTotalPercent == 100
+          ? "You've got everything! Ready to Go ✈️"
+          : `You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${numTotalPercent})%`}
       </em>
     </footer>
   );
